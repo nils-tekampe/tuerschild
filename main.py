bb82e91179c6f3ca3f0523bb346d0b3b30a77717
nils <nt@konfidas.de>
2019-06-22 14:00:23 +0200

    deep sleep tut

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import epaper7in5
-from machine import Pin, SPI
+import machine
+# from machine import Pin, SPI, deepsleep, lightsleep, WDT
 import framebuf
 import network
 import urequests
 from time import sleep_ms
 from welcome import welcome
 from stop import stop
+import time
 
 
+WATCHDOG_INTERVAL = 600000
 REFRESH_INTERVAL_MS = 1000
+SLEEP_TIME = 60000
+SCK = machine.Pin(18)
+MISO = machine.Pin(19)
+MOSI = machine.Pin(23)
+DC = machine.Pin(17)
+CS = machine.Pin(5)
+RST = machine.Pin(16)
+BUSY = machine.Pin(4)
+W = 640
+H = 384
+x = 0
+y = 0
+BLACK = 0
+WHITE = 1
 
 
 def do_connect():
@@ -23,51 +40,44 @@ def do_connect():
     print("network config :", wlan.ifconfig())
 
 
-def print_image(self, state):
-    SCK = Pin(18)
-    MISO = Pin(19)
-    MOSI = Pin(23)
-    DC = Pin(17)
-    CS = Pin(5)
-    RST = Pin(16)
-    BUSY = Pin(4)
-
-    W = 640
-    H = 384
-    x = 0
-    y = 0
-    BLACK = 0
-    WHITE = 1
-
-    spi = SPI(2, baudrate=20000000, polarity=0,
+def print_image(state):
+    spi = machine.SPI(2, baudrate=20000000, polarity=0,
               phase=0, sck=SCK, miso=MISO, mosi=MOSI)
 
-    print("starting program")
     e = epaper7in5.EPD(spi, CS, DC, RST, BUSY)
     e.init()
 
-    print("preparing frame buffer")
-    # buf = bytearray(W * H // 8)
-    # fb = framebuf.FrameBuffer(buf, W, H, framebuf.MONO_HLSB)
-
     if (state == "welcome"):
         print('Printing Willkommen')
-        # bufImage = willkommen
-        e.display_frame(willkommen)
+        e.display_frame(welcome)
     elif(state == "stop"):
         print('Printing Stop')
-        # bufImage = stop
         e.display_frame(stop)
 
     print("Print abgeschlossen")
 
+if (machine.reset_cause() == machine.DEEPSLEEP_RESET):
+    print("just woke up from deep sleep ")
+
 do_connect()
 state = ""
 
+wdt = machine.WDT(timeout=WATCHDOG_INTERVAL) 
+wdt.feed()
+
 while (True):
+    start = time.time()
     response = urequests.get('https://doorsign.konfidas.de/status.php')
+    print("current state:" + state)
     if (response.text != state):
         print("Received response: " + response.text)
         state = response.text
         print_image(response.text)
-    sleep(REFRESH_INTERVAL_MS)
+    response = urequests.get('https://doorsign.konfidas.de/confirmUpdate.php')
+    end = time.time()
+    print("time consumption of one cycle: "+str(end - start))
+    wdt.feed()
+    print("going to sleep now ")
+    machine.deepsleep(SLEEP_TIME)
+    # lightsleep(SLEEP_TIME)
+        
