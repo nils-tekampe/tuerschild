5e2f75024fcabb2c31532199c309e1f2cbfbd41a
nils <nt@konfidas.de>
2019-07-07 21:29:17 +0200

    Logging ergaenzt

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 
 #include <Arduino.h>
+#include <ArduinoLog.h>
 #include <HTTPClient.h>
 #include <SPI.h>
 #include <WiFi.h>
 #include <epd7in5.h>
 #include <images.h>
 
-const char *ssid = "alpha";
-const char *password = "halima@2003";
+const char *SSID = "alpha";
+const char *PASSWORD = "halima@2003";
 
 #define uS_TO_S_FACTOR                                                         \
   1000000                 /* Conversion factor for micro seconds to seconds */
@@ -20,27 +21,8 @@ RTC_DATA_ATTR String lastResult = "";
 
 #define LED_BUILTIN 2
 
-void setup() {
+void update() {
 
-  Serial.begin(9600);
-  Serial.print("Bootcounter: ");
-  Serial.println(bootCount);
-
-  bootCount++;
-
-  delay(4000);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-
-  Serial.println("Connected to the WiFi network");
-  pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void loop() {
   if ((WiFi.status() == WL_CONNECTED)) { // Check the current connection status
 
     HTTPClient http;
@@ -50,44 +32,78 @@ void loop() {
 
     if (httpCode > 0) { // Check for the returning code
       String payload = http.getString();
-
-      Serial.println("Der payload lautet:" + payload);
+      Log.notice("Der payload lautet: %s" CR, payload.c_str());
       if (payload != lastResult) {
-        Serial.println("Payload hat sich geändert. Wir zeichnen das Bild neu");
+        Log.notice("Payload hat sich geändert. Wir zeichnen das Bild neu" CR);
         Epd epd;
         if (epd.Init() != 0) {
-          Serial.print("e-Paper init failed");
+          Log.error("e-Paper init failed" CR);
           return;
         }
 
         if (payload == "welcome") {
-          Serial.println("Printing Willkommen");
+          Log.notice("Printing Willkommen" CR);
           epd.DisplayFrame(WELCOME_IMAGE);
         }
 
         else if (payload == "stop") {
-          Serial.println("Printing Stop");
+          Log.notice("Printing Stop" CR);
           epd.DisplayFrame(STOP_IMAGE);
         }
 
         lastResult = payload;
-        Serial.println("Bild wurde aktualisiert");
+        Log.notice("Bild wurde aktualisiert" CR);
       } else {
-        Serial.println(
-            "Der Status hat sich nicht geändert. Ich gehe wieder schlafen.");
+        Log.notice(
+            "Der Status hat sich nicht geändert. Ich gehe wieder schlafen." CR);
       }
     }
 
     else {
-      Serial.println("Error on HTTP request");
+      Log.error("Error on HTTP request" CR);
     }
-
-    http.begin(
-        "https://doorsign.konfidas.de/confirmUpdate.php"); // Specify the URL
 
     http.end(); // Free the resources
   }
+}
+
+void confirmUpdate() {
+  if ((WiFi.status() == WL_CONNECTED)) { // Check the current connection status
+    HTTPClient http;
+    http.begin(
+        "https://doorsign.konfidas.de/confirmUpdate.php"); // Specify the URL
+    int httpCode = http.GET();
+    Log.notice("confirmUpdate beendet." CR);
+    Log.notice("httpCode: %d" CR, httpCode);
+  }
+}
+
+void setup() {
+  delay(4000);
+  Serial.begin(115200);
+  // Log.begin(LOG_LEVEL_VERBOSE, &Serial);
+  Log.begin(LOG_LEVEL_SILENT, &Serial);
+
+  Log.notice("Bootcounter: %d" CR, bootCount);
+  Log.notice("lastResult: %s" CR, lastResult.c_str());
+
+  bootCount++;
+
+  WiFi.begin(SSID, PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Log.notice("Connection to WiFi...");
+  }
+
+  Log.notice("Connected to the WiFi network" CR);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  update();
+  confirmUpdate();
 
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   esp_deep_sleep_start();
 }
+
+void loop() {}
